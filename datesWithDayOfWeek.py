1ab8203ace010b5465aa7c714e82978a9b66ac51
Piotr Sobieraj <psobieraj+github@gmail.com>
2022-04-17 12:26:19 +0200

    Added dates to calendar

--- datesWithDayOfWeek.py
+++ datesWithDayOfWeek.py
@@ -1,10 +1,16 @@
 class DatesWithDayOfWeek:
     def __init__(self):
-        self.calendar = {}
+        self.calendar = dict()
+        self.daysOfWeek = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
         self.fill_calendar_dates()
 
     def fill_calendar_dates(self):
-        self.calendar["year"] = 1900
+        from datetime import date, timedelta
+        start_year = 2022
+        date = date(year=start_year, month=1, day=1)
+        while date.year <= start_year + 400:
+            self.calendar[(date.year, date.month, date.day)] = self.daysOfWeek[date.weekday()]
+            date += timedelta(days=1)
 
     def print(self):
         print(self.calendar)
