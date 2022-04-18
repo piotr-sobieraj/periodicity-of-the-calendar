f2ea713db4e81c43277725260ebd2c38bcee7a14
Piotr Sobieraj <psobieraj+github@gmail.com>
2022-04-18 12:34:25 +0200

    Working - misses data presentation

--- datesWithDayOfWeek.py
+++ datesWithDayOfWeek.py
@@ -1,10 +1,11 @@
 class DatesWithDayOfWeek:
     def __init__(self):
-        self.start_year = 1800
-        self.number_of_years = 10
+        self.start_year = 2020
+        self.number_of_years = 20
         self.calendar = dict()
         self.daysOfWeek = ("pon", "wto", "śro", "czw", "pią", "sob", "nie")
         self.fill_calendar_dates()
+        self.resul = []
 
     def fill_calendar_dates(self):
         from datetime import date, timedelta
@@ -25,12 +26,19 @@ class DatesWithDayOfWeek:
             current_year = day[0]
             current_month = day[1]
             current_day = day[2]
+            # print(current_year, current_month, current_day, dayOfWeek)
             last_year = list(self.calendar.keys())[-1][0]
-            # Looking towards by 1 year
-            # If dayOfWeeks are equal to each other - add to list
-            for year in range(current_year, last_year):
-                if self.calendar.get((year, current_month, current_day)) == dayOfWeek:
-                    print(day, (year, current_month, current_day),  self.calendar[year, current_month, current_day])
+            # # Looking towards by 1 year
+            # # If dayOfWeeks are equal to each other - add to list
+            difference_counter = 0
+            for year in range(current_year + 1, last_year):
+                difference_counter += 1
+                if self.get_day_of_week_by_date(current_day, current_month, year) == dayOfWeek:
+                    print(day, (year, current_month, current_day),  self.calendar[year, current_month, current_day], difference_counter)
                     break
 
+            # break
+
+    def get_day_of_week_by_date(self, current_day, current_month, year):
+        return self.calendar.get((year, current_month, current_day))
 
